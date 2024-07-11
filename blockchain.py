091fdc481e0753340aab91d7aaa9dd871b4bc240
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-07-11 11:42:56 +0300

    Add mining

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
 import json
+import random
 from datetime import datetime
 from hashlib import sha256
 
@@ -10,16 +11,18 @@ class Blockchain(object):
         
         # Create the genesis block
         print("Creating genesis block")
-        self.new_block()
+        self.chain.append(self.new_block())
         
-    def new_block(self, previous_hash = None):
+    def new_block(self):
         #Generate a new block and add it to the chain
         block = {
             'index': len(self.chain),
             'timestamp': datetime.utcnow().isoformat(),
             'transactions': self.pending_transactions,
-            'previous_hash': previous_hash,
+            'previous_hash': self.last_block['hash'] if self.last_block else None,
+            'nonce': format(random.getrandbits(64), 'x'),
         }
+        
         # Get the hash of this new block, and add it to the block
         block_hash = self.hash(block)
         block["hash"] = block_hash
@@ -27,10 +30,6 @@ class Blockchain(object):
         # Reset the list of pending transactions
         self.pending_transactions = []
         
-        # Add the block to the chain
-        self.chain.append(block)
-        
-        print(f"Created block {block['index']}")
         return block
     
     @staticmethod
@@ -39,6 +38,7 @@ class Blockchain(object):
         block_string = json.dumps(block, sort_keys=True).encode()
         return sha256(block_string).hexdigest()
     
+    @property
     def last_block(self):
         # Get the latest block in the chain
         return self.chain[-1] if self.chain else None
@@ -50,3 +50,20 @@ class Blockchain(object):
             "sender": sender,
             "amount": amount,
         })
+    
+    @staticmethod
+    def valid_block(block):
+        return block['hash'].startswith('0000')
+        
+    def proof_of_work(self):
+        while True:
+            new_block = self.new_block()
+            if self.valid_block(new_block):
+                break
+            
+        self.chain.append(new_block)
+        print("Found a new block: ", new_block)
+    
+    def valid_hash(self):
+        pass
+    
