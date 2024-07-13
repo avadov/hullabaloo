24cbc9aed9142cc7b6047034ea928f70935d4796
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-07-13 19:58:01 +0300

    A node

---
+++ node.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from hullabaloo.blockchain import Blockchain
+from hullabaloo.pool import ConnectionPool
+from hullabaloo.peers import P2PProtocol
+from hullabaloo.server import Server
+
+blockchain = Blockchain()
+# Our pool for peers
+connection_pool = ConnectionPool()
+
+# Instantiate the server
+server = Server(blockchain, connection_pool, P2PProtocol)
+
+async def main():
+    # Start the server
+    await server.listen()
+    
+asyncio.run(main())
