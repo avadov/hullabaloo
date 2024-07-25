9a0821858667e65a75c8b8557cfe0f10204ed6c1
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-07-25 12:09:33 +0300

    The Blockchain class

--- hullabaloo/blockchain.py
+++ hullabaloo/blockchain.py
@@ -1,35 +1,57 @@
+import math
+import asyncio
 import json
 import random
-from datetime import datetime
+from time import time
 from hashlib import sha256
+
+import structlog
+
+logger = structlog.getLogger("blockchain")
 
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.pending_transactions = []
+        self.target = "0000fffffffffffffffffffffffffffffff" + \
+        "fffffffffffffffffffffffffffff"
         
         # Create the genesis block
         print("Creating genesis block")
         self.chain.append(self.new_block())
         
     def new_block(self):
-        #Generate a new block and add it to the chain
-        block = {
-            'index': len(self.chain),
-            'timestamp': datetime.utcnow().isoformat(),
-            'transactions': self.pending_transactions,
-            'previous_hash': self.last_block['hash'] if self.last_block else None,
-            'nonce': format(random.getrandbits(64), 'x'),
-        }
-        
-        # Get the hash of this new block, and add it to the block
-        block_hash = self.hash(block)
-        block["hash"] = block_hash
+        # Generate a new block and add it to the chain
+        block = self.create_block(
+            height=len(self.chain),
+            transactions=self.pending_transactions,
+            previous_hash=self.last_block['hash'] if self.last_block else None,
+            nonce=format(random.getrandbits(64), 'x'),
+            target=self.target,
+            timestamp=time(),
+        )
         
         # Reset the list of pending transactions
         self.pending_transactions = []
         
+        return block
+    
+    @staticmethod
+    def create_block(height, transactions, previous_hash,
+                     nonce, target, timestamp=None):
+        block = {
+            "height": height,
+            "transactions": transactions,
+            "previous_hash": previous_hash,
+            "nonce": nonce,
+            "target": target,
+            "timestamp": timestamp or time(),
+        }
+        
+        # Get the hash of this new block and add it to the block
+        block_string = json.dumps(block, sort_keys=True).encode()
+        block["hash"] = sha256(block_string).hexdigest()
         return block
     
     @staticmethod
@@ -40,20 +62,60 @@ class Blockchain(object):
     
     @property
     def last_block(self):
-        # Get the latest block in the chain
+        # Get the last block in the chain
         return self.chain[-1] if self.chain else None
     
-    def new_transaction(self, sender, recipient, amount):
-        # Add a new transaction to the list of pending transactions 
-        self.pending_transactions.append({
-            "recipient": recipient,
-            "sender": sender,
-            "amount": amount,
-        })
+    def valid_block(self, block):
+        # Check if a block's hash is less than the target
+        return block['hash'] < self.target
     
-    @staticmethod
-    def valid_block(block):
-        return block['hash'].startswith('0000')
+    def add_block(self, block):
+        # TODO: Add proper validation logic
+        self.chain.append(block)
+        
+    def recalculate_target(self, block_index):
+        """
+        Returns the number we need to get below to mine a block
+        """
+        # Check if we need to recalculate the target
+        if block_index % 10 == 0:
+            # Expected time span of 10 blocks 
+            expected_timespan = 10 * 10
+            
+            # Calculate the actual time span
+            actual_timespan = self.chain[-1].["timestamp"] - \
+                self.chain[-10][timestamp]
+            
+            # Figure out what the offset is
+            ratio = actual_timespan / expected_timespan
+            
+            # Adjust the ratio to not be too extreme
+            ratio = max(0.25, ratio)
+            ratio = min(4.00, ratio)
+            
+            # Calculate the new target by multiplying the current 
+            # one by the ratio
+            new_target = int(self.target, 16) * ratio
+            
+            self.target = format(math.floor(new_target), "x").zfill(64)
+            logger.info(f"Calculated new mining target:{self.target}")
+        return self.target
+    
+    async def get_blocks_after_timestamp(self, timestamp):
+        for index, block in enumerate(self.chain):
+            if timestamp < block["timestamp"]:
+                return self.chain[index:]
+            
+    async def mine_new_block(self):
+        self.recalculate_target(self.last_block["index"] + 1)
+        while True:
+            new_block = self.new_block()
+            if self.valid_block(new_block):
+                break
+            await asyncio.sleep(0)
+        
+        self.chain.append(new_block)
+        logger.info("Found a new block: ", new_block)
         
     def proof_of_work(self):
         while True:
