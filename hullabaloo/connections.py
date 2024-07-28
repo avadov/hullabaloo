ea0f15d387e898465581eb857b25cb332ef4565e
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-07-28 19:42:02 +0300

    An outline of the ConnectionPool class

---
+++ hullabaloo/connections.py
@@ -0,0 +1,26 @@
+
+class ConnectionPool:
+    def __init__(self):
+        pass
+    
+    def broadcast(self, message):
+        # Broadcast a message to all connected peers
+        pass
+    
+    @staticmethod
+    def get_address_string(writer):
+        # Get a peer's ip:port
+        pass
+    
+    def add_peer(self, writer):
+        # Add a peer to our connection pool 
+        pass
+    
+    def remove_peer(self, writer):
+        # Remove a peer from the connection pool
+        pass
+    
+    def get_alive_peers(self, count):
+        # Return some connected peers
+        pass
+    
