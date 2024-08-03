116db502c66533b2c8a524ff660a146434dc3b78
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-08-03 20:22:48 +0300

    The ConnectionPool class

--- hullabaloo/connections.py
+++ hullabaloo/connections.py
@@ -1,26 +1,39 @@
+import structlog
+from more_itertools import take
+
+logger = structlog.getLogger(__name__)
+
 
 class ConnectionPool:
     def __init__(self):
-        pass
+        self.connection_pool = dict()
     
     def broadcast(self, message):
         # Broadcast a message to all connected peers
-        pass
+        for user in self.connection_pool:
+            user.write(f"{message}".encode())
     
     @staticmethod
     def get_address_string(writer):
         # Get a peer's ip:port
-        pass
+        ip = writer.address["ip"]
+        port = writer.address["port"]
+        return f"{ip}:{port}"
     
     def add_peer(self, writer):
         # Add a peer to our connection pool 
-        pass
+        address = self.get_address_string(writer)
+        self.connection_pool[address] = writer
+        logger.info("Added new peer to pool", address=address)
     
     def remove_peer(self, writer):
         # Remove a peer from the connection pool
-        pass
+        address = self.get_address_string(writer)
+        self.connection_pool.pop(address)
+        logger.info("Removed peer from pool", address=address)
     
     def get_alive_peers(self, count):
-        # Return some connected peers
-        pass
+        # Return count number of connected peers from the pool
+        # TODO: Sort peers by most active
+        return take(count, self.connection_pool.items())
     
